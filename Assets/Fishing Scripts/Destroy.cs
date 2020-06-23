02832af96dd28d71a2422d00ed9dc287ecd48386
jdvrossi <jdvrossi@uao.edu.co>
2020-06-22 23:23:43 -0500

    Pesca Magic al 81.5%

--- Assets/Fishing Scripts/Destroy.cs
+++ Assets/Fishing Scripts/Destroy.cs
@@ -5,29 +5,39 @@ using UnityEngine.UI;
 
 public class Destroy : MonoBehaviour
 {
- 
-    
+    private int score;
+    public Text Puntaje;
+
+    void Start()
+    {
+        score = 0;
+    }
+
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "PezV")
         { //object has the Pickup tag assigned.
 
             Destroy(collision.gameObject);
-            Debug.Log("Funciona");
+            score += 2;
         }
         else if (collision.gameObject.tag == "Pez_Rojo")
         { //object has the Pickup tag assigned.
 
-
-            Debug.Log("Gonorrea");
+            Destroy(collision.gameObject);
+            score -= 1;
         }
         else if (collision.gameObject.tag == "Pez_Amarillo")
         { //object has the Pickup tag assigned.
 
-
-            Debug.Log("Hpta");
+            Destroy(collision.gameObject);
+            score -= 1;
         }
 
+    }
+    void Update()
+    {
+        Puntaje.text = "Puntaje 1: " + score.ToString();
     }
 
 
